7f74f69b9cd9e2c75f89fbb45fb6f79ac908c762
Avighnash <avighnash@gmail.com>
2016-07-24 19:31:02 -0700

    Added class for GUI interact

---
+++ src/main/java/us/universalpvp/kit/event/listeners/GuiInteract.java
@@ -0,0 +1,9 @@
+package us.universalpvp.kit.event.listeners;
+
+/**
+ * Created by avigh on 7/24/2016.
+ */
+public class GuiInteract {
+
+    //TODO: Add PlayerInteractEvent listening for clicks in GUI
+}
